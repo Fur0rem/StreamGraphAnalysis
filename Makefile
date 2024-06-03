bcf08caf97ff28b74c5fd3e94c9c8d27befa0546
Fur0rem <pierre.baumann.n71@gmail.com>
2024-06-03 10:04:24 +0200

    test template finished

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 CC = gcc
 SRC_DIR = src
+BIN_DIR = bin
 DEBUG_FLAGS = -g -O0
 RELEASE_FLAGS = -O3
 FLAGS = $(RELEASE_FLAGS)
@@ -7,4 +8,4 @@ CFLAGS = -Wall -Wextra -Werror -std=c99 -pedantic $(FLAGS)
 LDFLAGS = -lm
 
 interval:
-	$(CC) $(CFLAGS) -c -o interval.o $(SRC_DIR)/interval.c $(LDFLAGS)
+	$(CC) $(CFLAGS) -c -o $(BIN_DIR)/interval.o $(SRC_DIR)/interval.c $(LDFLAGS)
--- tests/test.c
+++ tests/test.c
@@ -1,22 +1,38 @@
 #include "test.h"
+#include "../src/utils.h"
 
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdbool.h>
 
 bool test(const char* suite_name, ...) {
+    printf(" -- Running test suite : " TEXT_BOLD"%s"TEXT_RESET " --\n", suite_name);
     // For each test in the suite
     va_list args;
     va_start(args, suite_name);
     Test* test;
+    bool success = true;
     while ((test = va_arg(args, Test*))) {
         // Run the test
-        printf("Running test %s\n", test->name);
+        printf("%s : ", test->name);
         if (!test->fn()) {
-            printf("Test failed\n");
-            return false;
+            printf(TEXT_RED"FAIL\n"TEXT_RESET);
+            success = false;
         }
+        else {
+            printf(TEXT_GREEN"SUCESS\n"TEXT_RESET);
+        }
+        
     }
     va_end(args);
-    return true;
+
+    printf("-- " TEXT_BOLD "%s : ", suite_name);
+    if (success) {
+        printf(TEXT_GREEN "SUCCESS");
+    }
+    else {
+        printf(TEXT_RED "FAIL");
+    }
+    printf(TEXT_RESET " --\n");
+    return success;
 }
