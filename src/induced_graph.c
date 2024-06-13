928b1aa812a500ae508aa9fbeaa1eb36ae6c9383
Fur0rem <pierre.baumann.n71@gmail.com>
2024-06-13 15:24:44 +0200

    Link presents at time t added, will have to come back to induced_graph.c/.h later

--- src/induced_graph.c
+++ src/induced_graph.c
@@ -3,28 +3,19 @@
 #include <stddef.h>
 #include <stdio.h>
 
-SGA_NodesPresentAtT SGA_get_nodes_present_at_t(StreamGraph* stream_graph, TimeId t) {
-	size_t current_event = SGA_KeyMomentsTable_find_time_index(&stream_graph->key_moments, t);
-	SGA_NodesPresentAtT nodes_present_at_t = {
-		.stream_graph = stream_graph, .current_event = current_event, .current_node = 0};
-	return nodes_present_at_t;
-}
-
-SGA_LinksPresentAtT SGA_get_links_present_at_t(StreamGraph* stream_graph, TimeId t) {
-	size_t current_event = SGA_KeyMomentsTable_find_time_index(&stream_graph->key_moments, t);
-	SGA_LinksPresentAtT links_present_at_t = {
-		.stream_graph = stream_graph, .current_event = current_event, .current_link = 0};
-	return links_present_at_t;
-}
+// TODO : Lots of copy-paste here and in its .h file. Refactor this somehow without making it unreadable
+// TODO : Links work fully (tested in the extreme cases such as 0 links present), so use those as a reference for the
+// nodes
 
 size_t* SGA_NodesPresentAtT_next_after_disappearence(SGA_NodesPresentAtT* nodes_iter) {
 
 	// If the current event has 0 nodes
 	while (*SGA_Event_access_nb_nodes(nodes_iter->stream_graph->events.events[nodes_iter->current_event]) == 0) {
-		nodes_iter->current_event++;
 		if (nodes_iter->current_event == nodes_iter->stream_graph->events.nb_events - 1) {
 			return NULL;
 		}
+		nodes_iter->current_event++;
+		nodes_iter->current_node = 0;
 	}
 	// If you're the last node of the event
 	size_t* return_val;
@@ -47,13 +38,6 @@ size_t* SGA_NodesPresentAtT_next_after_disappearence(SGA_NodesPresentAtT* nodes_
 // Returns NULL if there are no more nodes present at time t
 size_t* SGA_NodesPresentAtT_next_before_disappearance(SGA_NodesPresentAtT* nodes_iter) {
 
-	// If the current event has 0 nodes
-	while (*SGA_Event_access_nb_nodes(nodes_iter->stream_graph->events.events[nodes_iter->current_event]) == 0) {
-		nodes_iter->current_event--;
-		if (nodes_iter->current_event == 0) {
-			return NULL;
-		}
-	}
 	// If you're the last node of the event
 	size_t* return_val;
 	if (nodes_iter->current_node ==
@@ -70,31 +54,67 @@ size_t* SGA_NodesPresentAtT_next_before_disappearance(SGA_NodesPresentAtT* nodes
 		nodes_iter->current_event--;
 		nodes_iter->current_node = 0;
 	}
+	// If the current event has 0 nodes
+	while (*SGA_Event_access_nb_nodes(nodes_iter->stream_graph->events.events[nodes_iter->current_event]) == 0) {
+		if (nodes_iter->current_event == 0) {
+			return NULL;
+		}
+		nodes_iter->current_event--;
+		nodes_iter->current_node = 0;
+	}
 	return_val = SGA_Event_access_nth_node(nodes_iter->stream_graph->events.events[nodes_iter->current_event],
 										   nodes_iter->current_node);
 	nodes_iter->current_node++;
 	return return_val;
 }
 
-size_t* SGA_NodesPresentAtT_next(SGA_NodesPresentAtT* nodes_iter) {
-	if (nodes_iter->current_event < nodes_iter->stream_graph->events.disappearance_index) {
-		return SGA_NodesPresentAtT_next_before_disappearance(nodes_iter);
+SGA_NodesPresentAtT SGA_get_nodes_present_at_t(StreamGraph* stream_graph, TimeId t) {
+	size_t current_event = SGA_KeyMomentsTable_find_time_index(&stream_graph->key_moments, t);
+	SGA_NodesPresentAtT nodes_present_at_t = {
+		.stream_graph = stream_graph, .current_event = current_event, .current_node = 0};
+	if (current_event >= stream_graph->events.disappearance_index) {
+		nodes_present_at_t.next = (size_t * (*)(void*)) SGA_NodesPresentAtT_next_after_disappearence;
 	}
 	else {
-		return SGA_NodesPresentAtT_next_after_disappearence(nodes_iter);
+		nodes_present_at_t.next = (size_t * (*)(void*)) SGA_NodesPresentAtT_next_before_disappearance;
 	}
+	return nodes_present_at_t;
 }
 
-// Returns NULL if there are no more nodes present at time t
-size_t* SGA_LinksPresentAtT_next(SGA_LinksPresentAtT* links_iter) {
-	// If the current event has 0 nodes
-	while (*SGA_Event_access_nb_nodes(links_iter->stream_graph->events.events[links_iter->current_event]) == 0) {
-		links_iter->current_event--;
-		if (links_iter->current_event == 0) {
+// Okay so next_after i have to put the skip loop before and in the next_before i have to put it after
+
+size_t* SGA_LinksPresentAtT_next_after_disappearence(SGA_LinksPresentAtT* links_iter) {
+
+	// If the current event has 0 links
+	while (*SGA_Event_access_nb_links(links_iter->stream_graph->events.events[links_iter->current_event]) == 0) {
+		if (links_iter->current_event == links_iter->stream_graph->events.nb_events - 1) {
 			return NULL;
 		}
+		links_iter->current_event++;
+		links_iter->current_link = 0;
 	}
-	// If you're the last node of the event
+	// If you're the last link of the event
+	size_t* return_val;
+	if (links_iter->current_link ==
+		*SGA_Event_access_nb_links(links_iter->stream_graph->events.events[links_iter->current_event])) {
+		// If you're the last event
+		if (links_iter->current_event == links_iter->stream_graph->events.nb_events - 1) {
+			return NULL;
+		}
+
+		links_iter->current_event++;
+		links_iter->current_link = 0;
+	}
+	return_val = SGA_Event_access_nth_link(links_iter->stream_graph->events.events[links_iter->current_event],
+										   links_iter->current_link);
+	links_iter->current_link++;
+	return return_val;
+}
+
+// Returns NULL if there are no more links present at time t
+size_t* SGA_LinksPresentAtT_next_before_disappearance(SGA_LinksPresentAtT* links_iter) {
+
+	// If you're the last link of the event
 	size_t* return_val;
 	if (links_iter->current_link ==
 		*SGA_Event_access_nb_links(links_iter->stream_graph->events.events[links_iter->current_event])) {
@@ -102,11 +122,21 @@ size_t* SGA_LinksPresentAtT_next(SGA_LinksPresentAtT* links_iter) {
 		if (links_iter->current_event == 0) {
 			return NULL;
 		}
+
 		// If your event has deletion (presence mask bit 0)
 		if (!SGA_BitArray_is_one(links_iter->stream_graph->events.presence_mask, links_iter->current_event - 1)) {
 			return NULL;
 		}
-		// Otherwise, go to the previous event
+		links_iter->current_event--;
+		links_iter->current_link = 0;
+	}
+
+	// If the current event has 0 links
+	while (*SGA_Event_access_nb_links(links_iter->stream_graph->events.events[links_iter->current_event]) == 0) {
+		if ((links_iter->current_event == 0) ||
+			(!SGA_BitArray_is_one(links_iter->stream_graph->events.presence_mask, links_iter->current_event - 1))) {
+			return NULL;
+		}
 		links_iter->current_event--;
 		links_iter->current_link = 0;
 	}
@@ -114,4 +144,17 @@ size_t* SGA_LinksPresentAtT_next(SGA_LinksPresentAtT* links_iter) {
 										   links_iter->current_link);
 	links_iter->current_link++;
 	return return_val;
+}
+
+SGA_LinksPresentAtT SGA_get_links_present_at_t(StreamGraph* stream_graph, TimeId t) {
+	size_t current_event = SGA_KeyMomentsTable_find_time_index(&stream_graph->key_moments, t);
+	SGA_LinksPresentAtT links_present_at_t = {
+		.stream_graph = stream_graph, .current_event = current_event, .current_link = 0};
+	if (current_event >= stream_graph->events.disappearance_index) {
+		links_present_at_t.next = (size_t * (*)(void*)) SGA_LinksPresentAtT_next_after_disappearence;
+	}
+	else {
+		links_present_at_t.next = (size_t * (*)(void*)) SGA_LinksPresentAtT_next_before_disappearance;
+	}
+	return links_present_at_t;
 }
--- src/induced_graph.h
+++ src/induced_graph.h
@@ -4,30 +4,36 @@
 #include "stream_graph.h"
 #include <stddef.h>
 
-// typedef size_t NodesPresentAtTimeIter;
-// typedef size_t LinksPresentAtTimeIter;
-
 typedef struct {
 	StreamGraph* stream_graph;
 	size_t current_event;
 	size_t current_node;
+	// Function that takes itself and returns the next node (size_t*)
+	// Obliged to put void* because of the circular dependency
+	size_t* (*next)(void*);
 } SGA_NodesPresentAtT;
+
+SGA_NodesPresentAtT SGA_get_nodes_present_at_t(StreamGraph* stream_graph, TimeId t);
+
+#define FOR_EACH_NODE_AT_TIME(stream_graph, iterated, t)                                                               \
+	SGA_NodesPresentAtT nodes_present_at_t = SGA_get_nodes_present_at_t(stream_graph, t);                              \
+	for (size_t * (iterated) = nodes_present_at_t.next(&nodes_present_at_t); (iterated) != NULL;                       \
+		 (iterated) = nodes_present_at_t.next(&nodes_present_at_t))
 
 typedef struct {
 	StreamGraph* stream_graph;
 	size_t current_event;
 	size_t current_link;
+	// Function that takes itself and returns the next node (size_t*)
+	// Obliged to put void* because of the circular dependency
+	size_t* (*next)(void*);
 } SGA_LinksPresentAtT;
 
-SGA_NodesPresentAtT SGA_get_nodes_present_at_t(StreamGraph* stream_graph, TimeId t);
 SGA_LinksPresentAtT SGA_get_links_present_at_t(StreamGraph* stream_graph, TimeId t);
 
-size_t* SGA_NodesPresentAtT_next(SGA_NodesPresentAtT* nodes_iter);
-size_t* SGA_LinksPresentAtT_next(SGA_LinksPresentAtT* links_iter);
-
-#define FOR_EACH_NODE_AT_TIME(stream_graph, iterated, t)                                                               \
-	SGA_NodesPresentAtT nodes_present_at_t = SGA_get_nodes_present_at_t(stream_graph, t);                              \
-	for (size_t * (iterated) = SGA_NodesPresentAtT_next(&nodes_present_at_t); (iterated) != NULL;                      \
-		 (iterated) = SGA_NodesPresentAtT_next(&nodes_present_at_t))
+#define FOR_EACH_LINK_AT_TIME(stream_graph, iterated, t)                                                               \
+	SGA_LinksPresentAtT links_present_at_t = SGA_get_links_present_at_t(stream_graph, t);                              \
+	for (size_t * (iterated) = links_present_at_t.next(&links_present_at_t); (iterated) != NULL;                       \
+		 (iterated) = links_present_at_t.next(&links_present_at_t))
 
 #endif // SGA_INDUCED_GRAPH_H
