1efd4e2c065bbc8788fd01e05f426f723ce4d7b1
Fur0rem <pierre.baumann.n71@gmail.com>
2024-06-13 15:24:44 +0200

    Link presents at time t added, will have to come back to induced_graph.c/.h later

--- tests/induced_graph.c
+++ tests/induced_graph.c
@@ -6,7 +6,6 @@ bool test_nodes_at_time() {
 	StreamGraph sg = SGA_StreamGraph_from_file("tests/test_data/S.txt");
 	FOR_EACH_NODE_AT_TIME(&sg, node, 20) {
 		printf("Node : %zu : %s\n", *node, sg.node_names[*node]);
-		sleep(1);
 	}
 	SGA_StreamGraph_destroy(&sg);
 	return true;
@@ -16,7 +15,6 @@ bool test_nodes_at_time_2() {
 	StreamGraph sg = SGA_StreamGraph_from_file("tests/test_data/S.txt");
 	FOR_EACH_NODE_AT_TIME(&sg, node, 60) {
 		printf("Node : %zu : %s\n", *node, sg.node_names[*node]);
-		sleep(1);
 	}
 	SGA_StreamGraph_destroy(&sg);
 	return true;
@@ -26,7 +24,47 @@ bool test_nodes_at_time_3() {
 	StreamGraph sg = SGA_StreamGraph_from_file("tests/test_data/S.txt");
 	FOR_EACH_NODE_AT_TIME(&sg, node, 100) {
 		printf("Node : %zu : %s\n", *node, sg.node_names[*node]);
-		sleep(1);
+	}
+	SGA_StreamGraph_destroy(&sg);
+	return true;
+}
+
+bool test_links_at_time() {
+	StreamGraph sg = SGA_StreamGraph_from_file("tests/test_data/S.txt");
+	FOR_EACH_LINK_AT_TIME(&sg, link, 20) {
+		printf("Link : %zu (%zu - %zu)\n", *link, sg.links.links[*link].nodes[0], sg.links.links[*link].nodes[1]);
+		printf("\t(%s - %s)\n", sg.node_names[sg.links.links[*link].nodes[0]],
+			   sg.node_names[sg.links.links[*link].nodes[1]]);
+	}
+	SGA_StreamGraph_destroy(&sg);
+	return true;
+}
+
+bool test_links_at_time_2() {
+	StreamGraph sg = SGA_StreamGraph_from_file("tests/test_data/S.txt");
+	FOR_EACH_LINK_AT_TIME(&sg, link, 60) {
+		printf("Link : %zu (%s -> %s)\n", *link, sg.node_names[sg.links.links[*link].nodes[0]],
+			   sg.node_names[sg.links.links[*link].nodes[1]]);
+	}
+	SGA_StreamGraph_destroy(&sg);
+	return true;
+}
+
+bool test_links_at_time_3() {
+	StreamGraph sg = SGA_StreamGraph_from_file("tests/test_data/S.txt");
+	FOR_EACH_LINK_AT_TIME(&sg, link, 100) {
+		printf("Link : %zu (%s -> %s)\n", *link, sg.node_names[sg.links.links[*link].nodes[0]],
+			   sg.node_names[sg.links.links[*link].nodes[1]]);
+	}
+	SGA_StreamGraph_destroy(&sg);
+	return true;
+}
+
+bool test_links_at_time_4() {
+	StreamGraph sg = SGA_StreamGraph_from_file("tests/test_data/S.txt");
+	FOR_EACH_LINK_AT_TIME(&sg, link, 40) {
+		printf("Link : %zu (%s -> %s)\n", *link, sg.node_names[sg.links.links[*link].nodes[0]],
+			   sg.node_names[sg.links.links[*link].nodes[1]]);
 	}
 	SGA_StreamGraph_destroy(&sg);
 	return true;
@@ -37,6 +75,10 @@ int main() {
 		&(Test){"nodes_at_time",	 test_nodes_at_time  },
 		  &(Test){"nodes_at_time_2", test_nodes_at_time_2},
 		&(Test){"nodes_at_time_3", test_nodes_at_time_3},
+		  &(Test){"links_at_time",   test_links_at_time	 },
+		&(Test){"links_at_time_2", test_links_at_time_2},
+		  &(Test){"links_at_time_3", test_links_at_time_3},
+		&(Test){"links_at_time_4", test_links_at_time_4},
 		  NULL
 	  };
 
