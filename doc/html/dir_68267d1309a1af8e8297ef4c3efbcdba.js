510685b289b78fc72c9a34171e0e75650b702697
Fur0rem <pierre.baumann.n71@gmail.com>
2024-06-27 14:07:03 +0200

    Changed the layout of the documentation html

---
+++ doc/html/dir_68267d1309a1af8e8297ef4c3efbcdba.js
@@ -0,0 +1,17 @@
+var dir_68267d1309a1af8e8297ef4c3efbcdba =
+[
+    [ "stream", "dir_090521d29e73b40f7fc0a63254d8965e.html", "dir_090521d29e73b40f7fc0a63254d8965e" ],
+    [ "stream_graph", "dir_63002fc154149f1f3feb0459c59bcf6a.html", "dir_63002fc154149f1f3feb0459c59bcf6a" ],
+    [ "bit_array.h", "bit__array_8h.html", "bit__array_8h" ],
+    [ "hashset.h", "hashset_8h_source.html", null ],
+    [ "induced_graph.h", "induced__graph_8h.html", "induced__graph_8h" ],
+    [ "interval.h", "interval_8h_source.html", null ],
+    [ "iterators.h", "iterators_8h.html", "iterators_8h" ],
+    [ "metrics.h", "metrics_8h.html", "metrics_8h" ],
+    [ "stream.h", "stream_8h_source.html", null ],
+    [ "stream_functions.h", "stream__functions_8h_source.html", null ],
+    [ "stream_graph.h", "stream__graph_8h_source.html", null ],
+    [ "units.h", "units_8h_source.html", null ],
+    [ "utils.h", "utils_8h_source.html", null ],
+    [ "vector.h", "vector_8h_source.html", null ]
+];
