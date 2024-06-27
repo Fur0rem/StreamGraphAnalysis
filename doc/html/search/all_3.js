f46063b15f3636a6fc8dae8006a2fd61e38cba2f
Fur0rem <pierre.baumann.n71@gmail.com>
2024-06-27 11:22:12 +0200

    Documentation for induced streamgraph

--- doc/html/search/all_3.js
+++ doc/html/search/all_3.js
@@ -1,8 +1,5 @@
 var searchData=
 [
-  ['informationcache_0',['InformationCache',['../structInformationCache.html',1,'']]],
-  ['interval_1',['Interval',['../structInterval.html',1,'']]],
-  ['intervalsiteratordata_2',['IntervalsIteratorData',['../structIntervalsIteratorData.html',1,'']]],
-  ['intervalsset_3',['IntervalsSet',['../structIntervalsSet.html',1,'']]],
-  ['iterators_2eh_4',['iterators.h',['../iterators_8h.html',1,'']]]
+  ['get_5flinks_5fpresent_5fat_5ft_0',['get_links_present_at_t',['../induced__graph_8h.html#ad410e24ad786fca6ceee4dd5d5bfb707',1,'induced_graph.c']]],
+  ['get_5fnodes_5fpresent_5fat_5ft_1',['get_nodes_present_at_t',['../induced__graph_8h.html#a04871219907d813f646d24a7f49d811a',1,'induced_graph.c']]]
 ];
--- doc/html/search/all_9.js
+++ doc/html/search/all_9.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['timesiterator_0',['TimesIterator',['../structTimesIterator.html',1,'']]],
-  ['timesiterator_5fintersection_1',['TimesIterator_intersection',['../iterators_8h.html#a9907f8eeea980f6996d18bbdfd01424e',1,'iterators.c']]],
-  ['timesiterator_5funion_2',['TimesIterator_union',['../iterators_8h.html#af1c011e4d83aece24cb4d3096a051264',1,'iterators.c']]],
-  ['total_5ftime_5fof_3',['total_time_of',['../iterators_8h.html#aba80f0a57ae795e7f431bfba2d162a08',1,'iterators.c']]]
+  ['stream_0',['Stream',['../structStream.html',1,'']]],
+  ['streamfunctions_1',['StreamFunctions',['../structStreamFunctions.html',1,'']]],
+  ['streamgraph_2',['StreamGraph',['../structStreamGraph.html',1,'']]],
+  ['streamgraphanalysis_3',['StreamGraphAnalysis',['../index.html',1,'']]]
 ];
