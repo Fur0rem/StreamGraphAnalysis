99a9c23fcab40f90f4dc477cfa085a87c889d5fd
Fur0rem <pierre.baumann.n71@gmail.com>
2024-06-27 17:15:13 +0200

    ChunkStreamSmall neighbours + nodes present + links present

--- tests/metrics.c
+++ tests/metrics.c
@@ -462,6 +462,76 @@ bool test_chunk_stream_small_nodes_set() {
 	return true;
 }
 
+bool test_chunk_stream_small_neighbours_of_node() {
+	StreamGraph sg = StreamGraph_from_file("tests/test_data/S.txt");
+	NodeIdVector nodes = NodeIdVector_with_capacity(2);
+	NodeIdVector_push(&nodes, 0);
+	NodeIdVector_push(&nodes, 1);
+	NodeIdVector_push(&nodes, 3);
+
+	LinkIdVector links = LinkIdVector_with_capacity(4);
+	LinkIdVector_push(&links, 0);
+	LinkIdVector_push(&links, 1);
+	LinkIdVector_push(&links, 2);
+	LinkIdVector_push(&links, 3);
+
+	Stream st = CSS_from(&sg, nodes.array, links.array, Interval_from(0, 100), nodes.size, links.size);
+	StreamFunctions funcs = STREAM_FUNCS(funcs, &st);
+	NodesIterator nodes_iter = funcs.nodes_set(st.stream);
+	FOR_EACH_NODE(node_id, nodes_iter) {
+		LinksIterator neighbours_iter = funcs.neighbours_of_node(st.stream, node_id);
+		printf("NODE %zu\n", node_id);
+		FOR_EACH_LINK(link_id, neighbours_iter) {
+			printf("LINK %zu (%zu, %zu)\n", link_id, sg.links.links[link_id].nodes[0],
+				   sg.links.links[link_id].nodes[1]);
+		}
+	}
+
+	ChunkStreamSmall_destroy(st);
+	StreamGraph_destroy(sg);
+	return true;
+}
+
+bool test_chunk_stream_small_times_node_present() {
+	StreamGraph sg = StreamGraph_from_file("tests/test_data/S.txt");
+	NodeIdVector nodes = NodeIdVector_with_capacity(3);
+	NodeIdVector_push(&nodes, 0);
+	NodeIdVector_push(&nodes, 1);
+	NodeIdVector_push(&nodes, 3);
+
+	LinkIdVector links = LinkIdVector_with_capacity(4);
+	LinkIdVector_push(&links, 0);
+	LinkIdVector_push(&links, 1);
+	LinkIdVector_push(&links, 2);
+	LinkIdVector_push(&links, 3);
+
+	Stream st = CSS_from(&sg, nodes.array, links.array, Interval_from(20, 80), nodes.size, links.size);
+	StreamFunctions funcs = STREAM_FUNCS(funcs, &st);
+	NodesIterator nodes_iter = funcs.nodes_set(st.stream);
+	FOR_EACH_NODE(node_id, nodes_iter) {
+		TimesIterator times_iter = funcs.times_node_present(st.stream, node_id);
+		printf("NODE %zu : ", node_id);
+		FOR_EACH_TIME(interval, times_iter) {
+			printf("[%lu, %lu] U ", interval.start, interval.end);
+		}
+		printf("\n");
+	}
+
+	LinksIterator links_iter = funcs.links_set(st.stream);
+	FOR_EACH_LINK(link_id, links_iter) {
+		TimesIterator times_iter = funcs.times_link_present(st.stream, link_id);
+		printf("LINK %zu : ", link_id);
+		FOR_EACH_TIME(interval, times_iter) {
+			printf("[%lu, %lu] U ", interval.start, interval.end);
+		}
+		printf("\n");
+	}
+
+	ChunkStreamSmall_destroy(st);
+	StreamGraph_destroy(sg);
+	return true;
+}
+
 // TEST_METRIC_F(compactness, 26.0 / 40.0, S)
 
 int main() {
@@ -508,6 +578,8 @@ int main() {
 		&(Test){"cache",									 test_cache									},
 
 		&(Test){"chunk_stream_small_nodes_set",				test_chunk_stream_small_nodes_set			 },
+		&(Test){"chunk_stream_small_neighbours_of_node",	 test_chunk_stream_small_neighbours_of_node	   },
+		&(Test){"chunk_stream_small_times_node_present",	 test_chunk_stream_small_times_node_present	   },
 
 		NULL,
 	};
