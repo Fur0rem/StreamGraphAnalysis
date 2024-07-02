377c1e33512447b9759d465d31077002ac57e415
Fur0rem <pierre.baumann.n71@gmail.com>
2024-07-02 18:13:02 +0200

    Disabled potential wrong optimisations for benchmarking

--- benchmarks/benchmark.h
+++ benchmarks/benchmark.h
@@ -1,6 +1,17 @@
 #ifndef BENCHMARK_H
 #define BENCHMARK_H
 
-void benchmark(void (*function)(void), const char* name, int iterations);
+/// Macro to prevent the compiler from optimising the function
+// If the compiler is Clang
+#ifdef __clang__
+#	define DONT_OPTIMISE __attribute__((optnone))
+// Else, check if the compiler is GCC
+#elif defined(__GNUC__)
+#	define DONT_OPTIMISE __attribute__((optimize("O0")))
+#else
+#	define DONT_OPTIMISE
+#endif
+
+DONT_OPTIMISE void benchmark(void (*function)(void), const char* name, int iterations);
 
 #endif // BENCHMARK_H
