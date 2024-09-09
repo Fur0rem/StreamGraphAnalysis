a8b652c374e5b7cda52c101ab1b369e6e8e5877b
Fur0rem <pierre.baumann.n71@gmail.com>
2024-09-09 02:10:48 +0200

    Optimised external format -> internal format with size prediction of the output

--- benchmarks/stream.c
+++ benchmarks/stream.c
@@ -22,49 +22,47 @@ DONT_OPTIMISE void init_events() {
 
 int main() {
 
-	char* filename	= "data/S_concat_L.txt";
+	char* filename	= "data/S_external.txt";
 	external_format = read_file(filename);
-	benchmark(external_to_internal, "S_concat_L external_to_internal", 1);
+	benchmark(external_to_internal, "S external_to_internal", 5);
 	internal_format = InternalFormat_from_External_str(external_format);
-	benchmark(load_from_internal, "S_concat_L load_from_internal", 1);
+	benchmark(load_from_internal, "S load_from_internal", 5);
 	sg = StreamGraph_from_string(internal_format);
-	benchmark(init_events, "S_concat_L init_events", 1);
+	benchmark(init_events, "S init_events", 5);
 	free(external_format);
 	free(internal_format);
 	StreamGraph_destroy(sg);
 
 	filename		= "data/LS_90.txt";
 	external_format = read_file(filename);
-	benchmark(external_to_internal, "LS_90 external_to_internal", 1);
+	benchmark(external_to_internal, "LS_90 external_to_internal", 5);
 	internal_format = InternalFormat_from_External_str(external_format);
-	benchmark(load_from_internal, "LS_90 load_from_internal", 1);
+	benchmark(load_from_internal, "LS_90 load_from_internal", 5);
 	sg = StreamGraph_from_string(internal_format);
-	benchmark(init_events, "LS_90 init_events", 1);
+	benchmark(init_events, "LS_90 init_events", 5);
 	free(external_format);
 	free(internal_format);
 	StreamGraph_destroy(sg);
 
 	filename		= "data/primaryschool_3125_transformed.txt";
 	external_format = read_file(filename);
-	benchmark(external_to_internal, "primaryschool external_to_internal", 1);
+	benchmark(external_to_internal, "primaryschool external_to_internal", 5);
 	internal_format = InternalFormat_from_External_str(external_format);
-	benchmark(load_from_internal, "primaryschool load_from_internal", 1);
+	benchmark(load_from_internal, "primaryschool load_from_internal", 5);
 	sg = StreamGraph_from_string(internal_format);
-	benchmark(init_events, "primaryschool init_events", 1);
+	benchmark(init_events, "primaryschool init_events", 5);
 	free(external_format);
 	free(internal_format);
 	StreamGraph_destroy(sg);
 
 	filename		= "data/facebooklike_1_transformed.txt";
 	external_format = read_file(filename);
-	benchmark(external_to_internal, "facebooklike external_to_internal", 1);
+	benchmark(external_to_internal, "facebooklike external_to_internal", 5);
 	internal_format = InternalFormat_from_External_str(external_format);
-	benchmark(load_from_internal, "facebooklike load_from_internal", 1);
+	benchmark(load_from_internal, "facebooklike load_from_internal", 5);
 	sg = StreamGraph_from_string(internal_format);
-	benchmark(init_events, "facebooklike init_events", 1);
+	benchmark(init_events, "facebooklike init_events", 5);
 	free(external_format);
 	free(internal_format);
 	StreamGraph_destroy(sg);
-
-	return 0;
 }
